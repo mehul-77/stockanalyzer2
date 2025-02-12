ef5561b705d152df695c5cef486e93e8b01f4be3
Mehul <156403084+mehul-77@users.noreply.github.com>
2025-02-12 15:29:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from newsapi import NewsApiClient
 # -----------------------------------------
 # Load the trained Random Forest model
 # -----------------------------------------
-model_path = '/mnt/data/random_forest_model.pkl'
+model_path = 'random_forest_model.pkl'
 model = joblib.load(model_path)
 
 # -----------------------------------------
